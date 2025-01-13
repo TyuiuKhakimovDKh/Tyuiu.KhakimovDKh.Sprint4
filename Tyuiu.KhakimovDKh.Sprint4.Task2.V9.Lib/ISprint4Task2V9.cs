d8a1349ded18a90d9da686b3621c8c936749e525
Khakimov Doston <Khakimovmma12@gmail.com>
2025-01-13 19:04:04 +0500

    Task2

---
+++ Tyuiu.KhakimovDKh.Sprint4.Task2.V9.Lib/ISprint4Task2V9.cs
@@ -0,0 +1,6 @@
+﻿namespace Tyuiu.KhakimovDKh.Sprint4.Task2.V9.Lib
+{
+    public interface ISprint4Task2V9
+    {
+    }
+}
---
+++ Tyuiu.KhakimovDKh.Sprint4.Task2.V9/Program.cs
@@ -0,0 +1,54 @@
+﻿using Tyuiu.KhakimovDKh.Sprint4.Task2.V9.Lib;
+
+
+namespace Tyuiu.KhakimovDKh.Sprint4.Task2.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил:Хакимов Д.Х. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт 4                                                                *");
+            Console.WriteLine("* Тема: Одномерные массивы (ввод с клавиатуры)                            *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнил: Хакимов Достонбек Хисравович | ИИПб-24-2                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 14 элементов заполненный         *");
+            Console.WriteLine("* случайными в диапазоне от 1 до 7 подсчитать произведение четных         *");
+            Console.WriteLine("* элементов массива.                                                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫНЕ:                                                       *");
+            Console.WriteLine("***************************************************************************");
+
+            int len;
+            Console.WriteLine("Введите количество элменетов массива: ");
+            len = Convert.ToInt32(Console.ReadLine());
+            int[] numsArray = new int[len];
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                numsArray[i] = rand.Next(1, 7);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("массив: ");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine(numsArray[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(numsArray);
+            Console.WriteLine(res);
+        }
+    }
+}
